3c12c5e72430ac60f7d60435117429133b2e0cb2
Jean-Marie Porchet <jm@jmporchet.ch>
2014-11-02 23:34:28 +0100

    implement the computer move

--- ttt.rb
+++ ttt.rb
@@ -15,7 +15,7 @@ def check_for_win (moves)
   #return nothing by default
 end
 
-def tests
+def run_tests
   #game in progress
   current_moves = {1=>nil, 2=>'o', 3=>nil, 4=>nil, 5=>'x', 6=>nil, 7=>nil, 8=>'o', 9=>'x' }
   puts check_for_win current_moves
@@ -25,14 +25,14 @@ def tests
   puts check_for_win current_moves
 
   #player WINNING_MOVES
-  current_moves = {1=>'o', 2=>'x', 3=>'o', 4=>nil, 5=>'x', 6=>nil, 7=>nil, 8=>'x', 9=>nil }
+  current_moves = {1=>'x', 2=>'x', 3=>'x', 4=>nil, 5=>'o', 6=>nil, 7=>nil, 8=>'o', 9=>nil }
   puts check_for_win current_moves
 end
 
 loop do
   #ask for position
-  puts "Positions " + current_moves.select { |i,m| m != nil }.keys.to_s + " are already taken. Which position? (1-9, x for tests)"
-  pos = gets.chomp
+  puts "Positions " + current_moves.select { |index,move| move != nil }.keys.to_s + " are already taken. Which position? (1-9, 0 for tests)"
+  pos = gets.chomp.to_i
 
   case pos
   when 1..9
@@ -40,11 +40,17 @@ loop do
     if current_moves.fetch(pos.to_i) != nil
       puts "spot already taken"
     else
-      current_moves.fetch(pos.to_i) = pos
+      #update the player move
+      current_moves.store(pos.to_i, 'x')
+      #get the remaining free spots and add one random 'o' for the computer move
+      available_moves = current_moves.select { |index,move| move == nil }
+      binding.pry
+      computer_move = available_moves.keys.sample(1).first
+      current_moves.store(computer_move, 'o')
     end
-  when 'x'
-    tests
-end
+  when 0
+    run_tests
+  end
 end
 
 
